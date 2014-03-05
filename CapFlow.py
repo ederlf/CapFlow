13f81381737e4c3d75aa42b8fb970a0153aaa527
ederlf <ederlf@cpqd.com.br>
2014-03-04 21:38:38 -0800

    Update CapFlow file with the most recent code

--- CapFlow.py
+++ CapFlow.py
@@ -1,66 +1,55 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
+# Python
+import collections
+
+# Ryu
 from ryu.base import app_manager
-from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, HANDSHAKE_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.controller import ofp_event
+from ryu.lib.packet import ethernet, ipv4, tcp, udp
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.ofproto import ether
+from ryu.ofproto import ofproto_v1_3
+
+# Us
+import config
+import util
+
+class Proto(object):
+    ETHER_IP = 0x800
+    ETHER_ARP = 0x806
+    IP_UDP = 17
+    IP_TCP = 6
+    TCP_HTTP = 80
+    UDP_DNS = 53
 
 
 class CapFlow(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-
     def __init__(self, *args, **kwargs):
         super(CapFlow, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        self.mac_to_port = collections.defaultdict(dict)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        print "Clear rule table"
+        util.delete_flow(datapath, parser.OFPMatch())
 
-        # install table-miss flow entry: Drop
-        #
-        match = parser.OFPMatch() 
-        actions = []       
-        self.add_flow(datapath, 0, match, actions)
+        # Send everything to ctrl
+        print "Install sending to controller rule"
+        util.add_flow(datapath,
+            parser.OFPMatch(),
+            [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)],
+            priority=2,
+        )
 
-        #Rule to direct DNS traffic to the internet
-        match = parser.OFPMatch(eth_type=0x800, ip_proto=17, udp_src=53)
-        actions = [parser.OFPActionOutput(2)]
-        self.add_flow(datapath, 100, match, actions)
+        # So we don't need to learn auth server location
+        # TODO: this assumes we are controlling only a single switch!
+        self.mac_to_port[datapath.id][config.AUTH_SERVER_MAC] = config.AUTH_SERVER_PORT
 
-        #Flow to redirect HTTP traffic
-        match = parser.OFPMatch(eth_type=0x800, ip_proto=6, tcp_src=80)
-        actions = [parser.OFPActionOutput(2)]
-        self.add_flow(datapath, 100, match, actions)
-
-    def add_flow(self, datapath, priority, match, actions):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
-        datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -73,33 +62,159 @@ class CapFlow(app_manager.RyuApp):
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        dst = eth.dst
-        src = eth.src
+        nw_dst = eth.dst
+        nw_src = eth.src
 
         dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.logger.info("packet in %s %s %s %s", dpid, nw_src, nw_dst, in_port)
 
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+        if nw_src not in self.mac_to_port[dpid]:
+            print "New client: dpid", dpid, "mac", nw_src, "port", in_port
+            self.mac_to_port[dpid][nw_src] = in_port
+            print "Installing *->%s forwarding rule" % nw_src
+            # This enables all traffic addressed to the client to go there
+            # FIXME: do we really want to enable this for an unauthenticated host?
+            util.add_flow(datapath,
+                parser.OFPMatch(
+                    eth_dst=nw_src,
+                ),
+                [parser.OFPActionOutput(in_port),],
+                priority=10,
+                msg=msg,
+            )
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
+        # pass ARP through, defaults to flooding if destination unknown
+        if eth.ethertype == Proto.ETHER_ARP:
+            print "ARP"
+            port = self.mac_to_port[dpid].get(nw_dst, ofproto.OFPP_FLOOD)
 
-        actions = [parser.OFPActionOutput(out_port)]
+            out = parser.OFPPacketOut(
+                    datapath=datapath,
+                    buffer_id=msg.buffer_id,
+                    in_port=in_port,
+                    actions=[parser.OFPActionOutput(port)],
+                    data=msg.data,
+                )
+            datapath.send_msg(out)
+            return
 
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            self.add_flow(datapath, 1, match, actions)
+        # Non-ARP traffic to unknown destination is dropped
+        if nw_dst not in self.mac_to_port[dpid]:
+            print "Unknown destination!",
+            return
 
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+        # We know destination
+        out_port = self.mac_to_port[dpid][nw_dst]
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+        # Helper functions (note: access variables from outer scope)
+        def install_l2_src_dst(nw_src, nw_dst, out_port):
+            util.add_flow(datapath,
+                parser.OFPMatch(
+                    eth_src=nw_src,
+                    eth_dst=nw_dst,
+                ),
+                [parser.OFPActionOutput(out_port),],
+                priority=100,
+                msg=msg,
+            )
+        def install_dns_fwd(nw_src, nw_dst, out_port):
+           util.add_flow(datapath,
+                parser.OFPMatch(
+                    eth_src=nw_src,
+                    eth_dst=nw_dst,
+                    eth_type=Proto.ETHER_IP,
+                    ip_proto=Proto.IP_UDP,
+                    udp_dst=Proto.UDP_DNS,
+                ),
+                [parser.OFPActionOutput(out_port)],
+                priority=100,
+                msg=msg,
+            )
+        def install_http_nat(nw_src, nw_dst, ip_src, ip_dst, tcp_src, tcp_dst):
+            # TODO: we do not change port right now so it might collide with other
+            # connections from the host. This is unlikely though
+
+            # Reverse rule goes first
+            util.add_flow(datapath,
+                parser.OFPMatch(
+                    in_port=config.AUTH_SERVER_PORT,
+                    eth_src=config.AUTH_SERVER_MAC,
+                    eth_dst=nw_src,
+                    eth_type=Proto.ETHER_IP,
+                    ip_proto=Proto.IP_TCP,
+                    ipv4_src=config.AUTH_SERVER_IP,
+                    ipv4_dst=ip_src,
+                    tcp_dst=tcp_src,
+                    tcp_src=tcp_dst,
+                ),
+                [parser.OFPActionSetField(ipv4_src=ip_dst),
+                 parser.OFPActionSetField(eth_src=nw_dst),
+                 parser.OFPActionOutput(in_port)
+                ],
+                priority=1000,
+            )
+            # Forward tule
+            util.add_flow(datapath,
+                parser.OFPMatch(
+                    in_port=in_port,
+                    eth_src=nw_src,
+                    eth_dst=nw_dst,
+                    eth_type=Proto.ETHER_IP,
+                    ip_proto=Proto.IP_TCP,
+                    ipv4_src=ip_src,
+                    ipv4_dst=ip_dst,
+                    tcp_dst=tcp_dst,
+                    tcp_src=tcp_src,
+                ),
+                [parser.OFPActionSetField(ipv4_dst=config.AUTH_SERVER_IP),
+                 parser.OFPActionSetField(eth_dst=config.AUTH_SERVER_MAC),
+                 parser.OFPActionOutput(config.AUTH_SERVER_PORT)
+                ],
+                priority=1000,
+                msg=msg,
+            )
+        def drop_unknown_ip(nw_src, nw_dst, ip_proto):
+            util.add_flow(datapath,
+                parser.OFPMatch(
+                    eth_src=nw_src,
+                    eth_dst=nw_dst,
+                    eth_type=Proto.ETHER_IP,
+                    ip_proto=ip_proto,
+                ),
+                [],
+                priority=10,
+                msg=msg,
+            )
+
+        # Logic itself
+        is_authenticated = False
+        # If the client is authenticated, install L2 MAC-MAC rule
+        if is_authenticated:
+            print "authenticated"
+            print "Installing", nw_src, "to", nw_dst, "bypass"
+            install_l2_src_dst(nw_src, nw_dst, out_port)
+            return
+
+        # Client is not authenticated
+        if eth.ethertype == Proto.ETHER_IP:
+            ip = pkt.get_protocols(ipv4.ipv4)[0]
+            if ip.proto == 1:
+                print "ICMP, ignore"
+                return
+            if ip.proto == Proto.IP_UDP:
+                _udp = pkt.get_protocols(udp.udp)[0]
+                if _udp.dst_port == Proto.UDP_DNS:
+                   print "Install DNS bypass"
+                   install_dns_fwd(nw_src, nw_dst, out_port)
+                else:
+                    print "Unknown UDP proto, ignore"
+                    return
+            elif ip.proto == Proto.IP_TCP:
+                _tcp = pkt.get_protocols(tcp.tcp)[0]
+                if _tcp.dst_port == Proto.TCP_HTTP:
+                    print "Is HTTP traffic, installing NAT entry"
+                    install_http_nat(nw_src,nw_dst, ip.src, ip.dst, _tcp.src_port, _tcp.dst_port)
+            else:
+                print "Unknown IP proto, dropping"
+                drop_unknown_ip(nw_src, nw_dst, ip.proto)
